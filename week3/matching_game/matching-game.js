03cac2c15144dd00eb63b180278293c2bf55ba18
Vishnu Raj <vishnu.rkv@gmail.com>
2018-11-21 14:26:17 +0530

    Added smileys
    Refined view

--- week3/matching_game/matching-game.js
+++ week3/matching_game/matching-game.js
@@ -1,34 +1,43 @@
 
 function startGame(){
   makeFaces();
-
 }
+
 
 function winner() {
-  alert("winner!")
+  alert("Winner! Winner!");
 }
+
+function gameOver(){
+  alert("Game Over!");
+  theBody = document.getElementById("theBody");
+  
+  while(theBody.firstChild){
+    theBody.removeChild(theBody.firstChild);
+  }
+}
+
 function removeOddOne(){
   odd_one = document.getElementById("oddOne");
   odd_one.parentNode.removeChild(odd_one);
 }
 function makeFaces(level){
     let count = 1;
-    //console.log(level);
-    //level++;
     var level = 10;
     box_one = document.getElementById("box1");
     box_two = document.getElementById("box2");
 
     while(count < level) {
-        // let smiley = document.createElement("IMG");
-        // smiley.setAttribute("src", "smiley.jpeg");
-        let smiley = document.createElement("div");
-        let smiley_two = document.createElement("div");
+        let smiley = document.createElement("IMG");
+        smiley.setAttribute("src", "https://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png");
+        let smiley_two = document.createElement("IMG");
+        smiley_two.setAttribute("src", "https://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png");
         top_position = Math.floor(Math.random()*500);
         left_position = Math.floor(Math.random()*500);
 
-        smiley.setAttribute("style", "background-color: yellow; position: absolute; border: solid; height: 15px; width: 15px;");
-        smiley_two.setAttribute("style", "background-color: yellow; position: absolute; border: solid; height: 15px; width: 15px;");
+        smiley.setAttribute("style", "position: absolute; ");
+        smiley.setAttribute("id", "wrong_choice")
+        smiley_two.setAttribute("style", "position: absolute; ");
         smiley.style.left = left_position+"px";
         smiley.style.top = top_position+"px";
 
@@ -41,18 +50,21 @@ function makeFaces(level){
         count++;
     }
 
-    let odd_one = document.createElement("div");
+    let odd_one = document.createElement("IMG");
+    odd_one.setAttribute("src", "https://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png");
     odd_one.setAttribute("id", "oddOne");
-    odd_one.setAttribute("style", "background-color: yellow; position: absolute; border: solid; height: 15px; width: 15px;");
+    odd_one.setAttribute("style", "position: absolute; ");
     top_position = Math.floor(Math.random()*500);
     left_position = Math.floor(Math.random()*500);
     odd_one.style.left = left_position+"px";
     odd_one.style.top = top_position+"px";
 
 
-    box_two.appendChild(odd_one);
+    box_one.appendChild(odd_one);
     odd_one.addEventListener("click", winner);
     odd_one.addEventListener("click", removeOddOne);
     odd_one.addEventListener("click", makeFaces);
+    wrong_choice.addEventListener("click", gameOver);
+
 
 }
